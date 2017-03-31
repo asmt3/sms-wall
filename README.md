ad9beb8bf11b5bc75edeceb6918cfa8cfeca9042
Alan Thomson <alan@projectmccoll.co.uk>
2017-04-01 00:46:57 +0200

    firebase backend

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # sms-wall
 Displays SMSs sent to a Twilio phone number and displays then in a slideshow
+
+
+
+# todo
+- read sms on frontend
+- set up twilio
+- set up webhook from twilio
+- deploy
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../config/firebase.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://smswall-7e973.firebaseio.com"
+});
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/sms', function(req, res, next){
+
+	var punData = {
+		content: 'text tesxt'
+	}
+
+	var punsRef = admin.database().ref("puns");
+	punsRef.push().set(punData).then(snapshot => {
+		res.send(punData);
+	})
+
+})
 
 module.exports = router;
